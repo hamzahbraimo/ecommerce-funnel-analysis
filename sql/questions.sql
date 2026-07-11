7db20b115dc7bfd709647a3cda18cbf176050446
Hamzah Braimo <hamzah.brm@gmail.com>
2026-07-11 19:02:16 +0200

    outliers removed

--- sql/questions.sql
+++ sql/questions.sql
@@ -1,1 +1,9 @@
 # 1. The most expensive brands per category
+SELECT
+    brand, 
+    category,
+    SUM(price) AS total,
+    RANK() OVER (ORDER BY SUM(price) DESC) AS rank_preco
+FROM events
+GROUP BY brand, category
+ORDER BY category, total DESC;
