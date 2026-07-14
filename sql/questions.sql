b32edc1f0dee608a176c93197d07558002cd91f6
Hamzah Braimo <hamzah.brm@gmail.com>
2026-07-14 14:52:05 +0200

    answered questions 4 and 5 + fixed filter on question 2

--- sql/questions.sql
+++ sql/questions.sql
@@ -21,6 +21,7 @@ WITH count_views AS (
         DENSE_RANK() OVER(PARTITION BY category ORDER BY COUNT(brand) DESC) AS ranking
     FROM events
     WHERE brand != 'Unknown'
+        AND event_type = 'view'
     GROUP BY brand, category
 )
 SELECT * FROM count_views
@@ -28,10 +29,41 @@ WHERE ranking BETWEEN 1 AND 3;
 
 
 -- 3. Top 25 users with the most expending
-SELECT 
-    user_id,
-    ROUND(SUM(price), 2) AS total_spent,
-    RANK() OVER (ORDER BY SUM(PRICE) DESC) AS ranking
-FROM events
-GROUP BY user_id
+WITH users AS (
+    SELECT 
+        user_id,
+        ROUND(SUM(price), 2) AS total_spent,
+        RANK() OVER (ORDER BY SUM(PRICE) DESC) AS ranking
+    FROM events
+    GROUP BY user_id
+)
+SELECT * FROM users
 LIMIT 25;
+
+
+-- 4. First viewed product per user in a session
+WITH users_views AS (
+    SELECT
+        user_id,
+        user_session,
+        CONCAT(brand, " (", category, ")") AS first_viewed_product,
+        ROW_NUMBER() OVER(PARTITION BY user_id, user_session) AS number 
+    FROM events
+    WHERE event_type = 'view'
+)
+SELECT
+    user_id,
+    user_session,
+    first_viewed_product
+FROM users_views
+WHERE number = 1;
+
+
+-- 5. Last event per session
+WITH events AS (
+    SELECT *,
+        ROW_NUMBER() OVER(PARTITION BY user_session ORDER BY event_time DESC) AS number   
+    FROM events
+)
+SELECT * FROM events 
+WHERE number = 1;
